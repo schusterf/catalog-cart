2c9cba7a7d8ccff38779f369c53fab39e1734c34
frankschuster <frank.schuster86@gmail.com>
2018-02-16 13:52:36 -0700

    Changed server script to also pull the display value from the question_choice table.

--- serverscript.js
+++ serverscript.js
@@ -15,7 +15,7 @@
 	var cartJS = new sn_sc.CartJS(cartName, '' + userID);
 
 	data.sys_id = $sp.getParameter('sys_id');
-	
+
 	if (input && input.action === "checkout_two") {
 		data.checkoutCartModal = $sp.getWidget('widget-modal', {embeddedWidgetId: 'sc-checkout', embeddedWidgetOptions: {cart: input.cart}, backdrop: 'static', keyboard: false, size: 'md'});
 		return;
@@ -51,7 +51,7 @@
 		newCart.setHidden(false);
 		newCart.loadCart(cart.getCartID());
 	}
-	
+
 	data.sys_properties = {
 		twostep: gs.getProperty("glide.sc.sp.twostep", "true") == 'true'
 	};
@@ -67,33 +67,40 @@
 		}
 	data.cartItems = data.cartItems.map(function (item) {
 		item.quantity = parseInt(item.quantity);
-		
+
 		return item;
 	})
-	
+
 	for(var i=0; i < data.cartItems.length; i++) {
 		data.cartItems[i].variables = getVariables(data.cartItems[i].cart_item_id);
 	}
-	
+
 	data.disable_req_for = sn_sc.CartJS.canViewRF();
 
 	data.saveCartModal = $sp.getWidget('widget-modal', {embeddedWidgetId: 'sc_save_bundle', embeddedWidgetOptions: {}});
 
-	
+
 	//retrieve variables from the item option table (until we can figure out how to use sn_sc.CartJS)
 	function getVariables(cartItem) {
 		var variables = {};
-		
+
 		var grItemOption = new GlideRecord("sc_item_option");
 		grItemOption.addQuery("cart_item", cartItem);
-		grItemOption.addQuery("item_option_new.name", "u_mr_job_name"); //name of the variable (better: do gs.getProperty() and write the value into a sys_property to retrieve from there)
+		grItemOption.addQuery("item_option_new.name", "storage"); //name of the variable (better: do gs.getProperty() and write the value into a sys_property to retrieve from there)
 		grItemOption.query();
 		if(grItemOption.next()) {
 			variables.question = grItemOption.getDisplayValue("item_option_new");
-			variables.value = grItemOption.getDisplayValue("value");
+			
+			//try to retrieve the display value for that variable from the question_choice table, otherwise fall back to the variable value (Example: QC value would be "64GB" vs variable value "sixtyfour").
+			var grQuestionChoice = new GlideRecord("question_choice");
+			if(grQuestionChoice.get("value", grItemOption.getValue("value"))) {				
+				variables.value = grQuestionChoice.getDisplayValue("text");
+			} else {				
+				variables.value = grItemOption.getDisplayValue("value");
+			}
 		}
-		
+
 		return variables;		
 	}
-	
+
 })();
